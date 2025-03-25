ef4fec0acc6934684d015a4af46909ddaa4c565d
rozlynd <rozlyn.df@gmail.com>
2025-03-25 10:37:57 +0100

    Specify DT evaluation with Inductive

--- coq/DT.v
+++ coq/DT.v
@@ -13,6 +13,14 @@ Section DecisionTrees.
            (t : testIndex (fs i))
            (dt1 dt2 : decisionTree).
 
+    Inductive DTSpec
+        (x : featureSpace fs) (c : class) : decisionTree -> Prop :=
+    | leafPath : DTSpec x c (Leaf c)
+    | nodePathLeft : forall i t dt1 dt2,
+        testFeature x i t = true -> DTSpec x c dt1 -> DTSpec x c (Node i t dt1 dt2)
+    | nodePathRight : forall i t dt1 dt2,
+        testFeature x i t = false -> DTSpec x c dt2 -> DTSpec x c (Node i t dt1 dt2).
+
     Fixpoint evalDT
         (dt : decisionTree)
         (x : featureSpace fs) : class :=
@@ -24,5 +32,21 @@ Section DecisionTrees.
             else
                 evalDT dt2 x
         end.
+
+    Theorem evalDTCorrect : forall (dt : decisionTree) (x : featureSpace fs) (c : class),
+        DTSpec x c dt <-> evalDT dt x = c.
+    Proof.
+        intros; split; intro H.
+        -   induction H as [
+                | i t dt1 dt2 Htest IH
+                | i t dt1 dt2 Htest IH ]; simpl;
+                try reflexivity;
+            rewrite Htest; assumption.
+        -   induction dt as [ c' | i t dt1 IH1 dt2 IH2 ]; simpl in H;
+                try (rewrite H; constructor);
+            destruct (testFeature x i t) eqn:Htest;
+                [constructor 2; try assumption; auto
+                |constructor 3; try assumption; auto].
+    Qed.
 
 End DecisionTrees.
