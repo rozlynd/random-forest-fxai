d03ada9f9c3cff25f154b424547de23d374855a8
rozlynd <rozlyn.df@gmail.com>
2026-01-09 14:39:04 +0100

    Module ExplanationProblem encapsulates prediction

--- coq/Xp.v
+++ coq/Xp.v
@@ -29,154 +29,147 @@ End FeatureSig.
 
 
 Module Type Classifier (F : FeatureSig) (K : UsualDecidableType).
-
     Parameter k : featureVec F.fs -> K.t.
-
 End Classifier.
 
-
-Module Explanations (Export F : FeatureSig) (K : UsualDecidableType) (Export C : Classifier F K).
-
-    Section ExplanationsMainDefs.
-
-        Context (v : featureVec fs).
-
-        Definition WCXp (X : S.t) : Prop :=
-            exists (v' : featureVec fs), equiv (S.compl X) v v' /\ k v <> k v'.
-
-        Definition WAXp (X : S.t) : Prop :=
-            forall (v' : featureVec fs), equiv X v v' -> k v = k v'.
-
-        Definition CXp (X : S.t) : Prop :=
-            WCXp X /\ forall X', S.Subset X' X -> WCXp X' -> S.Equal X' X.
-
-        Definition AXp (X : S.t) : Prop :=
-            WAXp X /\ forall X', S.Subset X' X -> WAXp X' -> S.Equal X' X.
-
-    End ExplanationsMainDefs.
+Module Type ClassifierInstance (F : FeatureSig).
+    Parameter v : featureVec F.fs.
+End ClassifierInstance.
 
 
-    Global Instance WCXp_compat : Proper (Logic.eq ==> S.Equal ==> iff) WCXp.
+Module Type ExplanationProblem := FeatureSig <+ UsualDecidableType <+ Classifier <+ ClassifierInstance.
+
+
+Module Explanations (Export E : ExplanationProblem).
+
+    Definition WCXp (X : S.t) : Prop :=
+        exists (v' : featureVec fs), equiv (S.compl X) v v' /\ k v <> k v'.
+
+    Definition WAXp (X : S.t) : Prop :=
+        forall (v' : featureVec fs), equiv X v v' -> k v = k v'.
+
+    Definition CXp (X : S.t) : Prop :=
+        WCXp X /\ forall X', S.Subset X' X -> WCXp X' -> S.Equal X' X.
+
+    Definition AXp (X : S.t) : Prop :=
+        WAXp X /\ forall X', S.Subset X' X -> WAXp X' -> S.Equal X' X.
+
+
+    Global Instance WCXp_compat : Proper (S.Equal ==> iff) WCXp.
     Proof.
-        intros v1 v2 HEv s1 s2 HEs; split; intros (v' & H1 & H2);
-        subst v2; exists v'; split; try (now rewrite <- ?HEs); now rewrite -> ?HEs.
+        intros s1 s2 HEs; split; intros (v' & H1 & H2);
+        exists v'; split; try (now rewrite <- ?HEs); now rewrite -> ?HEs.
     Qed.
 
-    Global Instance WCXAp_compat : Proper (Logic.eq ==> S.Equal ==> iff) WAXp.
+    Global Instance WCXAp_compat : Proper (S.Equal ==> iff) WAXp.
     Proof.
-        intros v1 v2 HEv s1 s2 HEs; split; intros H v' HE.
-        -   subst v2; apply H; now rewrite HEs.
-        -   subst v2; apply H; now rewrite <- HEs.
+        intros s1 s2 HEs; split; intros H v' HE; apply H;
+        try (now rewrite HEs); now rewrite <- HEs.
     Qed.
 
 End Explanations.
 
 
-Module ExplanationsFacts (Export F : FeatureSig) (K : UsualDecidableType) (Export C : Classifier F K).
+Module ExplanationsFacts (Export E : ExplanationProblem).
 
-    Module Xp := Explanations F K C.
+    Module Xp := Explanations E.
     Import Xp Props.
 
-    Section Facts.
 
-        (* Monotonicity *)
+    (* Monotonicity *)
 
-        Context (v : featureVec fs).
+    Theorem WCXp_monotonic :
+        forall (X1 X2 : S.t), S.Subset X1 X2 -> WCXp X1 -> WCXp X2.
+    Proof.
+        intros X1 X2 HSubs (v' & HEq & HDiff);
+        exists v'; split; try (now apply HDiff);
+        intros i Hi; apply Subset_compl in HSubs;
+        now apply HEq, HSubs.
+    Qed.
 
-        Theorem WCXp_monotonic :
-            forall (X1 X2 : S.t), S.Subset X1 X2 -> WCXp v X1 -> WCXp v X2.
-        Proof.
-            intros X1 X2 HSubs (v' & HEq & HDiff);
-            exists v'; split; try (now apply HDiff);
-            intros i Hi; apply Subset_compl in HSubs;
-            now apply HEq, HSubs.
-        Qed.
-
-        Theorem WAXp_monotonic :
-            forall (X1 X2 : S.t), S.Subset X1 X2 -> WAXp v X1 -> WAXp v X2.
-        Proof.
-            intros X1 X2 HSubs H v' HEq;
-            apply H; intros x Hx; now apply HEq, HSubs.
-        Qed.
+    Theorem WAXp_monotonic :
+        forall (X1 X2 : S.t), S.Subset X1 X2 -> WAXp X1 -> WAXp X2.
+    Proof.
+        intros X1 X2 HSubs H v' HEq;
+        apply H; intros x Hx; now apply HEq, HSubs.
+    Qed.
 
 
-        (* Sufficient conditions for minimality *)
+    (* Sufficient conditions for minimality *)
 
-        (* Same proof used twice; could be made more abstractly *)
-        Theorem CXp_mono :
-            forall (X : S.t),
-                ( WCXp v X /\ forall i, S.In i X -> ~ WCXp v (S.remove i X) )
-                    -> CXp v X.
-        Proof.
-            intros X (isWCXp & removeOneNotWCXp); split; try assumption;
-            intros X' HSubs isWCXp'; apply subset_antisym; try assumption;
-            intros i Hi; destruct (In_dec i X') as [| HN ]; try assumption;
-            exfalso; apply removeOneNotWCXp with (i := i); try assumption;
-            apply WCXp_monotonic with (X1 := X'); try assumption;
-            intros j Hj; apply S.remove_spec; split; try (now apply HSubs);
-            intro abs; now subst j.
-        Qed.
+    (* Same proof used twice; could be made more abstractly *)
+    Theorem CXp_mono :
+        forall (X : S.t),
+            ( WCXp X /\ forall i, S.In i X -> ~ WCXp (S.remove i X) )
+                -> CXp X.
+    Proof.
+        intros X (isWCXp & removeOneNotWCXp); split; try assumption;
+        intros X' HSubs isWCXp'; apply subset_antisym; try assumption;
+        intros i Hi; destruct (In_dec i X') as [| HN ]; try assumption;
+        exfalso; apply removeOneNotWCXp with (i := i); try assumption;
+        apply WCXp_monotonic with (X1 := X'); try assumption;
+        intros j Hj; apply S.remove_spec; split; try (now apply HSubs);
+        intro abs; now subst j.
+    Qed.
 
-        Theorem AXp_mono :
-            forall (X : S.t),
-                ( WAXp v X /\ forall i, S.In i X -> ~ WAXp v (S.remove i X) )
-                    -> AXp v X.
-        Proof.
-            intros X (isWAXp & removeOneNotWAXp); split; try assumption;
-            intros X' HSubs isWAXp'; apply subset_antisym; try assumption;
-            intros i Hi; destruct (In_dec i X') as [| HN ]; try assumption;
-            exfalso; apply removeOneNotWAXp with (i := i); try assumption;
-            apply WAXp_monotonic with (X1 := X'); try assumption;
-            intros j Hj; apply S.remove_spec; split; try (now apply HSubs);
-            intro abs; now subst j.
-        Qed.
+    Theorem AXp_mono :
+        forall (X : S.t),
+            ( WAXp X /\ forall i, S.In i X -> ~ WAXp (S.remove i X) )
+                -> AXp X.
+    Proof.
+        intros X (isWAXp & removeOneNotWAXp); split; try assumption;
+        intros X' HSubs isWAXp'; apply subset_antisym; try assumption;
+        intros i Hi; destruct (In_dec i X') as [| HN ]; try assumption;
+        exfalso; apply removeOneNotWAXp with (i := i); try assumption;
+        apply WAXp_monotonic with (X1 := X'); try assumption;
+        intros j Hj; apply S.remove_spec; split; try (now apply HSubs);
+        intro abs; now subst j.
+    Qed.
 
 
-        (* Duality *)
+    (* Duality *)
 
-        Theorem XpDual_compl_of_not_WCXp_is_WAXp :
-            forall (X : S.t), ~ WCXp v X <-> WAXp v (S.compl X).
-        Proof.
-            intros X; split; intro H.
-            -   intros v' Heq;
-                destruct (K.eq_dec (k v) (k v')); try assumption;
-                exfalso; apply H; exists v'; split; assumption.
-            -   intros (v' & H1 & H2); apply H2, H, H1.
-        Qed.
+    Theorem XpDual_compl_of_not_WCXp_is_WAXp :
+        forall (X : S.t), ~ WCXp X <-> WAXp (S.compl X).
+    Proof.
+        intros X; split; intro H.
+        -   intros v' Heq;
+            destruct (E.eq_dec (k v) (k v')); try assumption;
+            exfalso; apply H; exists v'; split; assumption.
+        -   intros (v' & H1 & H2); apply H2, H, H1.
+    Qed.
 
-        Theorem XpDual_compl_of_WCXp_is_not_WAXp :
-            forall (X : S.t), WCXp v X -> ~ WAXp v (S.compl X).
-        Proof.
-            intros X H abs; now rewrite <- XpDual_compl_of_not_WCXp_is_WAXp in abs.
-        Qed.
+    Theorem XpDual_compl_of_WCXp_is_not_WAXp :
+        forall (X : S.t), WCXp X -> ~ WAXp (S.compl X).
+    Proof.
+        intros X H abs; now rewrite <- XpDual_compl_of_not_WCXp_is_WAXp in abs.
+    Qed.
 
-        (* Can't exhibit a WCXp witness in constructive logic *)
-        Theorem XpDual_compl_of_WCXp_to_not_WAXp' :
-            forall (X : S.t), ~ WAXp v (S.compl X) -> ~ ~ WCXp v X.
-        Proof.
-            intros X H abs; apply H, XpDual_compl_of_not_WCXp_is_WAXp, abs.
-        Qed.
+    (* Can't exhibit a WCXp witness in constructive logic *)
+    Theorem XpDual_compl_of_WCXp_to_not_WAXp' :
+        forall (X : S.t), ~ WAXp (S.compl X) -> ~ ~ WCXp X.
+    Proof.
+        intros X H abs; apply H, XpDual_compl_of_not_WCXp_is_WAXp, abs.
+    Qed.
 
-        Theorem XpDual_uncompl_of_not_WCXp_is_WAXp :
-            forall (X : S.t), ~ WCXp v (S.compl X) <-> WAXp v X.
-        Proof.
-            intros X;
-            assert (H := XpDual_compl_of_not_WCXp_is_WAXp (S.compl X));
-            now rewrite compl_compl in H.
-        Qed.
+    Theorem XpDual_uncompl_of_not_WCXp_is_WAXp :
+        forall (X : S.t), ~ WCXp (S.compl X) <-> WAXp X.
+    Proof.
+        intros X;
+        assert (H := XpDual_compl_of_not_WCXp_is_WAXp (S.compl X));
+        now rewrite compl_compl in H.
+    Qed.
 
-        Theorem XpDual_uncompl_of_WCXp_is_not_WAXp :
-            forall (X : S.t), WCXp v (S.compl X) -> ~ WAXp v X.
-        Proof.
-            intros X H abs; now rewrite <- XpDual_uncompl_of_not_WCXp_is_WAXp in abs.
-        Qed.
+    Theorem XpDual_uncompl_of_WCXp_is_not_WAXp :
+        forall (X : S.t), WCXp (S.compl X) -> ~ WAXp X.
+    Proof.
+        intros X H abs; now rewrite <- XpDual_uncompl_of_not_WCXp_is_WAXp in abs.
+    Qed.
 
-        Theorem XpDual_uncompl_of_WCXp_is_not_WAXp' :
-            forall (X : S.t), ~ WAXp v X -> ~ ~ WCXp v (S.compl X).
-        Proof.
-            intros X H abs; apply H, XpDual_uncompl_of_not_WCXp_is_WAXp, abs.
-        Qed.
-
-    End Facts.
+    Theorem XpDual_uncompl_of_WCXp_is_not_WAXp' :
+        forall (X : S.t), ~ WAXp X -> ~ ~ WCXp (S.compl X).
+    Proof.
+        intros X H abs; apply H, XpDual_uncompl_of_not_WCXp_is_WAXp, abs.
+    Qed.
 
 End ExplanationsFacts.
